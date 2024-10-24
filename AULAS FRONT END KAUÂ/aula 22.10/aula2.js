d2925af25aede55330d6d4a12ed011c65cbe61ca
StrayChanutt <kauaschultzdasilva2@gmail.com>
2024-10-24 15:08:18 -0300

    Add files via upload

---
+++ AULAS FRONT END KAUÂ/aula 22.10/aula2.js
@@ -0,0 +1,21 @@
+let resultado1 = 3 + 4
+console.log(resultado1)
+
+resultado1 = (3 * 5)/2
+console.log(resultado1)
+
+resultado1 = (4 - 5) * -1
+console.log(resultado1)
+
+resultado1 = 234 % 5
+console.log(resultado1)
+
+let primeiroNumero = 5
+let segundoNumero = 4
+
+console.log(primeiroNumero === segundoNumero)
+console.log(primeiroNumero !== segundoNumero)
+console.log(primeiroNumero > segundoNumero)
+console.log(primeiroNumero < segundoNumero)
+
+let 
